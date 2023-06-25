5c2a8359fea98f3bc82fcdc6d4be9505599cd9ba
parthojha12 <112394456+parthojha12@users.noreply.github.com>
2023-06-25 22:54:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express = require("express");
-const app = require("./src/app");
-const mongoose = require("mongoose");
-const port = process.env.port || 4001;
+// This code sets up a server using Express.js and connects it to a MongoDB database using Mongoose.
 
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// Importing necessary modules
+const express = require("express"); // Importing Express module
+const app = require("./src/app"); // Importing custom app module
+const mongoose = require("mongoose"); // Importing Mongoose module
+const port = process.env.port || 4001; // Setting the port number
 
-// Connect to DATABASE
+// Middleware to parse JSON bodies and URL-encoded bodies
+app.use(express.json()); // Parsing JSON bodies
+app.use(express.urlencoded({ extended: false })); // Parsing URL-encoded bodies
+
+// Connecting to the database
 const DATABASE_URL =
-  "mongodb+srv://ojhaparth:test123@cluster0.9xedsar.mongodb.net/";
+"mongodb+srv://ojhaparth:test123@cluster0.9xedsar.mongodb.net/";
 
 mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+useNewUrlParser: true,
+useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on("error", (err) => console.log(err));
-db.once("open", () => console.log("connected to database"));
+db.on("error", (err) => console.log(err)); // Handling database connection errors
+db.once("open", () => console.log("connected to database")); // Logging successful database connection
 
-// Start Server
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+// Starting the server
+app.listen(port, () => console.log(`App listening on port ${port}!`)); // Logging the server start and listening on the specified port
